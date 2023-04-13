c3e4f323de5e1800c21d25c3ef5d7029cfcda855
Mohd Numan <48924562+one-numan@users.noreply.github.com>
2023-04-13 13:35:59 +0530

    Create Send_only_body_email.py

---
+++ Send_only_body_email.py
@@ -0,0 +1,26 @@
+
+def send_mail(sender,app_password,receiver, message):
+    try:
+        # creates SMTP session
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+
+        # start TLS for security
+        s.starttls()
+
+        # Authentication
+        s.login(sender, app_password)
+
+  
+
+        # sending the mail
+        s.sendmail(sender, receiver, message)
+        print("Email Send")
+        # terminating the session
+        s.quit()
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+
+send_mail(sender,app_password,receiver, message)
